70b4a43747557412786fedf37d2c17daaaa4e715
Aleksandar Stojanovic <sale4319@gmail.com>
2025-04-11 21:21:08 +0200

    chore: refactor for vercel deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,11 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link
-          rel="icon"
-          type="image/svg+xml"
-          href="/clue-hunt-next/favicon.svg"
-        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Audiowide&display=optional"
