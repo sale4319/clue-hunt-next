a0fd7f12e2eb9e76b5a74700c08745659a3d7a6d
aleksandar-stojanovic-ext <aleksandar.stojanovic.ext@adevinta.com>
2025-10-29 16:18:01 +0100

    chore: fix suspense issue

--- src/app/level/(routes)/five/page.tsx
+++ src/app/level/(routes)/five/page.tsx
@@ -2,11 +2,14 @@ import "server-only";
 
 import { Page } from "@app/page-component";
 import LevelFive from "../../_components/LevelFive";
+import { Suspense } from "react";
 
 export default function page() {
   return (
     <Page>
-      <LevelFive />
+      <Suspense fallback={<div>Loading...</div>}>
+        <LevelFive />
+      </Suspense>
     </Page>
   );
 }
