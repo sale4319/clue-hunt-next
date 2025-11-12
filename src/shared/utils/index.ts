7d0ed34f045a22c6fa250c00a57f17bec93acf78
aleksandar-stojanovic-ext <aleksandar.stojanovic.ext@adevinta.com>
2025-11-12 22:20:29 +0100

    refactor: reduce countdowntimer component

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,4 +1,5 @@
 export { calculateScore, getCompletionTimeInSeconds } from "./calculateScore";
+export { formatTimeFromMs, twoDigits } from "./formatTimeFromMs";
 export { getRoute } from "./getRoute";
 export { getTimeLeftDisplay } from "./getTimeLeftDisplay";
 export { useFeatureToggle } from "./useFeatureToggle";
